7b3ea90446cd39aa6011e185a352de2fda92fa12
masruhin <masruhin15201029@gmail.com>
2024-01-02 09:32:24 +0700

    ok

--- beranda.php
+++ beranda.php
@@ -1,168 +1,168 @@
 <?php include 'layout/header.php';?>
-  <!-- BEGIN: Body-->
+<!-- BEGIN: Body-->
 
-  <body class="vertical-layout vertical-menu-modern  navbar-floating footer-static  " data-open="click"
-    data-menu="vertical-menu-modern" data-col="">
-    <div class="wave"></div>
-    <div class="wave"></div>
-    <div class="wave"></div>
+<body class="vertical-layout vertical-menu-modern  navbar-floating footer-static  " data-open="click"
+  data-menu="vertical-menu-modern" data-col="">
+  <div class="wave"></div>
+  <div class="wave"></div>
+  <div class="wave"></div>
+  <p>asd</p>
+  <?php include 'layout/nav.php'?>
 
-    <?php include 'layout/nav.php'?>
-
-   <?php include 'layout/menu.php'?>
-    <!-- BEGIN: Content-->
-    <div class="app-content content ">
-      <div class="content-overlay"></div>
-      <div class="header-navbar-shadow"></div>
-      <div class="content-wrapper container-xxl p-0">
-        <div class="content-header row">
-        </div>
-        <div class="content-body">
-          <!-- Dashboard Ecommerce Starts -->
-          <section id="dashboard-ecommerce">
-            <div class="row match-height">
-              <!-- Medal Card -->
-              <div class="col-xl-3 col-md-6 col-12">
-                <div class="card card-congratulation-medal">
-                  <div class="card-body">
-                    <h3>Universitas</h3>
-                    <h5 class="mb-75 mt-2 pt-50">
-                      <a href="javascript:void(0);">Total Dokumen</a>
-                    </h5>
-                    <button type="button" class="btn btn-primary">Lihat</button>
-                    <!-- <img src="vendor/app-assets/images/illustration/badge.svg" class="congratulation-medal"
+  <?php include 'layout/menu.php'?>
+  <!-- BEGIN: Content-->
+  <div class="app-content content ">
+    <div class="content-overlay"></div>
+    <div class="header-navbar-shadow"></div>
+    <div class="content-wrapper container-xxl p-0">
+      <div class="content-header row">
+      </div>
+      <div class="content-body">
+        <!-- Dashboard Ecommerce Starts -->
+        <section id="dashboard-ecommerce">
+          <div class="row match-height">
+            <!-- Medal Card -->
+            <div class="col-xl-3 col-md-6 col-12">
+              <div class="card card-congratulation-medal">
+                <div class="card-body">
+                  <h3>Universitas</h3>
+                  <h5 class="mb-75 mt-2 pt-50">
+                    <a href="javascript:void(0);">Total Dokumen</a>
+                  </h5>
+                  <button type="button" class="btn btn-primary">Lihat</button>
+                  <!-- <img src="vendor/app-assets/images/illustration/badge.svg" class="congratulation-medal"
                       alt="Medal Pic" /> -->
-                  </div>
                 </div>
               </div>
-              <!--/ Medal Card -->
-
-              <!-- Medal Card -->
-              <div class="col-xl-3 col-md-6 col-12">
-                <div class="card card-congratulation-medal">
-                  <div class="card-body">
-                    <h3>FIKES</h3>
-                    <h5 class="mb-75 mt-2 pt-50">
-                      <a href="javascript:void(0);">Total Dokumen</a>
-                    </h5>
-                    <button type="button" class="btn btn-primary">Lihat</button>
-                    <!-- <img src="vendor/app-assets/images/illustration/badge.svg" class="congratulation-medal"
-                      alt="Medal Pic" /> -->
-                  </div>
-                </div>
-              </div>
-              <!--/ Medal Card -->
-
-              <!-- Medal Card -->
-              <div class="col-xl-3 col-md-6 col-12">
-                <div class="card card-congratulation-medal">
-                  <div class="card-body">
-                    <h3>FEB</h3>
-                    <h5 class="mb-75 mt-2 pt-50">
-                      <a href="javascript:void(0);">Total Dokumen</a>
-                    </h5>
-                    <button type="button" class="btn btn-primary">Lihat</button>
-                    <!-- <img src="vendor/app-assets/images/illustration/badge.svg" class="congratulation-medal"
-                      alt="Medal Pic" /> -->
-                  </div>
-                </div>
-              </div>
-              <!--/ Medal Card -->
-
-              <!-- Medal Card -->
-              <div class="col-xl-3 col-md-6 col-12">
-                <div class="card card-congratulation-medal">
-                  <div class="card-body">
-                    <h3>FIKOM</h3>
-                    <h5 class="mb-75 mt-2 pt-50">
-                      <a href="javascript:void(0);">Total Dokumen</a>
-                    </h5>
-                    <button type="button" class="btn btn-primary">Lihat</button>
-                    <!-- <img src="vendor/app-assets/images/illustration/badge.svg" class="congratulation-medal"
-                      alt="Medal Pic" /> -->
-                  </div>
-                </div>
-              </div>
-              <!--/ Medal Card -->
             </div>
-          </section>
-          <!-- Dashboard Ecommerce ends -->
+            <!--/ Medal Card -->
 
-          <!-- Dashboard Ecommerce Starts -->
-          <section id="dashboard-ecommerce">
-            <div class="row match-height">
-              <!-- Medal Card -->
-              <div class="col-xl-3 col-md-6 col-12">
-                <div class="card card-congratulation-medal">
-                  <div class="card-body">
-                    <h3>LPM</h3>
-                    <h5 class="mb-75 mt-2 pt-50">
-                      <a href="javascript:void(0);">Total Dokumen</a>
-                    </h5>
-                    <button type="button" class="btn btn-primary">Lihat</button>
-                    <!-- <img src="vendor/app-assets/images/illustration/badge.svg" class="congratulation-medal"
+            <!-- Medal Card -->
+            <div class="col-xl-3 col-md-6 col-12">
+              <div class="card card-congratulation-medal">
+                <div class="card-body">
+                  <h3>FIKES</h3>
+                  <h5 class="mb-75 mt-2 pt-50">
+                    <a href="javascript:void(0);">Total Dokumen</a>
+                  </h5>
+                  <button type="button" class="btn btn-primary">Lihat</button>
+                  <!-- <img src="vendor/app-assets/images/illustration/badge.svg" class="congratulation-medal"
                       alt="Medal Pic" /> -->
-                  </div>
                 </div>
               </div>
-              <!--/ Medal Card -->
-
-              <!-- Medal Card -->
-              <div class="col-xl-3 col-md-6 col-12">
-                <div class="card card-congratulation-medal">
-                  <div class="card-body">
-                    <h3>LP2M</h3>
-                    <h5 class="mb-75 mt-2 pt-50">
-                      <a href="javascript:void(0);">Total Dokumen</a>
-                    </h5>
-                    <button type="button" class="btn btn-primary">Lihat</button>
-                    <!-- <img src="vendor/app-assets/images/illustration/badge.svg" class="congratulation-medal"
-                      alt="Medal Pic" /> -->
-                  </div>
-                </div>
-              </div>
-              <!--/ Medal Card -->
-
-              <!-- Medal Card -->
-              <div class="col-xl-3 col-md-6 col-12">
-                <div class="card card-congratulation-medal">
-                  <div class="card-body">
-                    <h3>BAU</h3>
-                    <h5 class="mb-75 mt-2 pt-50">
-                      <a href="javascript:void(0);">Total Dokumen</a>
-                    </h5>
-                    <button type="button" class="btn btn-primary">Lihat</button>
-                    <!-- <img src="vendor/app-assets/images/illustration/badge.svg" class="congratulation-medal"
-                      alt="Medal Pic" /> -->
-                  </div>
-                </div>
-              </div>
-              <!--/ Medal Card -->
-
-              <!-- Medal Card -->
-              <div class="col-xl-3 col-md-6 col-12">
-                <div class="card card-congratulation-medal">
-                  <div class="card-body">
-                    <h3>BAAK</h3>
-                    <h5 class="mb-75 mt-2 pt-50">
-                      <a href="javascript:void(0);">Total Dokumen</a>
-                    </h5>
-                    <button type="button" class="btn btn-primary">Lihat</button>
-                    <!-- <img src="vendor/app-assets/images/illustration/badge.svg" class="congratulation-medal"
-                      alt="Medal Pic" /> -->
-                  </div>
-                </div>
-              </div>
-              <!--/ Medal Card -->
             </div>
-          </section>
-          <!-- Dashboard Ecommerce ends -->
+            <!--/ Medal Card -->
 
-        </div>
+            <!-- Medal Card -->
+            <div class="col-xl-3 col-md-6 col-12">
+              <div class="card card-congratulation-medal">
+                <div class="card-body">
+                  <h3>FEB</h3>
+                  <h5 class="mb-75 mt-2 pt-50">
+                    <a href="javascript:void(0);">Total Dokumen</a>
+                  </h5>
+                  <button type="button" class="btn btn-primary">Lihat</button>
+                  <!-- <img src="vendor/app-assets/images/illustration/badge.svg" class="congratulation-medal"
+                      alt="Medal Pic" /> -->
+                </div>
+              </div>
+            </div>
+            <!--/ Medal Card -->
+
+            <!-- Medal Card -->
+            <div class="col-xl-3 col-md-6 col-12">
+              <div class="card card-congratulation-medal">
+                <div class="card-body">
+                  <h3>FIKOM</h3>
+                  <h5 class="mb-75 mt-2 pt-50">
+                    <a href="javascript:void(0);">Total Dokumen</a>
+                  </h5>
+                  <button type="button" class="btn btn-primary">Lihat</button>
+                  <!-- <img src="vendor/app-assets/images/illustration/badge.svg" class="congratulation-medal"
+                      alt="Medal Pic" /> -->
+                </div>
+              </div>
+            </div>
+            <!--/ Medal Card -->
+          </div>
+        </section>
+        <!-- Dashboard Ecommerce ends -->
+
+        <!-- Dashboard Ecommerce Starts -->
+        <section id="dashboard-ecommerce">
+          <div class="row match-height">
+            <!-- Medal Card -->
+            <div class="col-xl-3 col-md-6 col-12">
+              <div class="card card-congratulation-medal">
+                <div class="card-body">
+                  <h3>LPM</h3>
+                  <h5 class="mb-75 mt-2 pt-50">
+                    <a href="javascript:void(0);">Total Dokumen</a>
+                  </h5>
+                  <button type="button" class="btn btn-primary">Lihat</button>
+                  <!-- <img src="vendor/app-assets/images/illustration/badge.svg" class="congratulation-medal"
+                      alt="Medal Pic" /> -->
+                </div>
+              </div>
+            </div>
+            <!--/ Medal Card -->
+
+            <!-- Medal Card -->
+            <div class="col-xl-3 col-md-6 col-12">
+              <div class="card card-congratulation-medal">
+                <div class="card-body">
+                  <h3>LP2M</h3>
+                  <h5 class="mb-75 mt-2 pt-50">
+                    <a href="javascript:void(0);">Total Dokumen</a>
+                  </h5>
+                  <button type="button" class="btn btn-primary">Lihat</button>
+                  <!-- <img src="vendor/app-assets/images/illustration/badge.svg" class="congratulation-medal"
+                      alt="Medal Pic" /> -->
+                </div>
+              </div>
+            </div>
+            <!--/ Medal Card -->
+
+            <!-- Medal Card -->
+            <div class="col-xl-3 col-md-6 col-12">
+              <div class="card card-congratulation-medal">
+                <div class="card-body">
+                  <h3>BAU</h3>
+                  <h5 class="mb-75 mt-2 pt-50">
+                    <a href="javascript:void(0);">Total Dokumen</a>
+                  </h5>
+                  <button type="button" class="btn btn-primary">Lihat</button>
+                  <!-- <img src="vendor/app-assets/images/illustration/badge.svg" class="congratulation-medal"
+                      alt="Medal Pic" /> -->
+                </div>
+              </div>
+            </div>
+            <!--/ Medal Card -->
+
+            <!-- Medal Card -->
+            <div class="col-xl-3 col-md-6 col-12">
+              <div class="card card-congratulation-medal">
+                <div class="card-body">
+                  <h3>BAAK</h3>
+                  <h5 class="mb-75 mt-2 pt-50">
+                    <a href="javascript:void(0);">Total Dokumen</a>
+                  </h5>
+                  <button type="button" class="btn btn-primary">Lihat</button>
+                  <!-- <img src="vendor/app-assets/images/illustration/badge.svg" class="congratulation-medal"
+                      alt="Medal Pic" /> -->
+                </div>
+              </div>
+            </div>
+            <!--/ Medal Card -->
+          </div>
+        </section>
+        <!-- Dashboard Ecommerce ends -->
+
       </div>
     </div>
-    <!-- END: Content-->
+  </div>
+  <!-- END: Content-->
 
-    <div class="sidenav-overlay"></div>
-    <div class="drag-target"></div>
-<?php include 'layout/footer.php'?>
+  <div class="sidenav-overlay"></div>
+  <div class="drag-target"></div>
+  <?php include 'layout/footer.php'?>
